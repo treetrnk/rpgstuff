46cf25375e12ab25fd70355ab3d03b9444c29665
Nathan Hare <nhare330@gmail.com>
2016-02-12 14:58:56 -0500

    Implement jQuery navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-angular.module('rpgApp', [])
-  .controller('rpgCon', function() {
-    var rpg = this;
+$(document).ready(function() {
+  
+  $('#body').load('about.html');
+
+  var include = function(replacedId, url) {
+    jQuery.get(url,function(data) {
+      $(replacedId).replaceWith(data);
+    });
+  };
+
+  $('#link-fatecore').click(function(){
+    include('#body','fatecore/index.html');
+    return false;
+  });
+
+  $('#link-storygen').click(function(){
+    include('#body','storygen/index.html');
+    return false;
+  });
+
+  $('#link-about').click(function(){
+    include('#body','about.html');
+    return false;
+  });
+
+});
